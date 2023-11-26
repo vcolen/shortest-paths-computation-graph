144d2a9cccdbf914250fbfd44e451603768952c1
VIctor <colen.victor@icloud.com>
2023-11-26 09:13:39 -0300

    add dijkstra's algorithm

--- Algorithms/Dijkstra.cpp
+++ Algorithms/Dijkstra.cpp
@@ -1,104 +1,85 @@
-// C++ program for Dijkstra's single source shortest path
-// algorithm. The program is for adjacency matrix
-// representation of the graph
+// https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+
 #include <iostream>
-using namespace std;
 #include <limits.h>
 
-// Number of vertices in the graph
-#define V 9
+using namespace std;
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+#define VERTICES_COUNT 9
+
+int getMinimumDistanceVertexIndex(int distances[], bool isShortestPathTreeSet[])
 {
+    int minimumDistance = INT_MAX;
+    int minimumDistanceIndex;
 
-    // Initialize min value
-    int min = INT_MAX, min_index;
+    for (int vertex = 0; vertex < VERTICES_COUNT; vertex++)
+        // Se não estiver na árvore (para evitar ciclos) e a distância for menor,
+        // esse se torna o novo vértice escolhido
+        if (!isShortestPathTreeSet[vertex] && distances[vertex] <= minimumDistance)
+            minimumDistance = distances[vertex], minimumDistanceIndex = vertex;
 
-    for (int v = 0; v < V; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-
-    return min_index;
+    return minimumDistanceIndex;
 }
 
-// A utility function to print the constructed distance
-// array
-void printSolution(int dist[])
+void printSolution(int distances[])
 {
-    cout << "Vertex \t Distance from Source" << endl;
-    for (int i = 0; i < V; i++)
-        cout << i << " \t\t\t\t" << dist[i] << endl;
+    cout << "Vértice \t Distância a partir da Fonte" << endl;
+    for (int i = 0; i < VERTICES_COUNT; i++)
+        cout << i << " \t\t\t\t" << distances[i] << endl;
 }
 
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
+void dijkstra(int graph[VERTICES_COUNT][VERTICES_COUNT], int source)
 {
-    int dist[V]; // The output array. dist[i] will hold the
-                 // shortest
-    // distance from src to i
+    int distances[VERTICES_COUNT]; // Contém as distâncias da fonte para os outros vértices
+    bool shortestPathTreeSet[VERTICES_COUNT];
 
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is
-                    // included in shortest
-    // path tree or shortest distance from src to i is
-    // finalized
+    // Inicializando os vetores
+    for (int i = 0; i < VERTICES_COUNT; i++)
+        distances[i] = INT_MAX, shortestPathTreeSet[i] = false;
 
-    // Initialize all distances as INFINITE and stpSet[] as
-    // false
-    for (int i = 0; i < V; i++)
-        dist[i] = INT_MAX, sptSet[i] = false;
+    distances[source] = 0;
 
-    // Distance of source vertex from itself is always 0
-    dist[src] = 0;
-
-    // Find shortest path for all vertices
-    for (int count = 0; count < V - 1; count++)
+    // Encontrar o caminho mais curto para todos os outros vértices
+    for (int count = 0; count < VERTICES_COUNT - 1; count++)
     {
-        // Pick the minimum distance vertex from the set of
-        // vertices not yet processed. u is always equal to
-        // src in the first iteration.
-        int u = minDistance(dist, sptSet);
+        int minimumDistanceVertexIndex = getMinimumDistanceVertexIndex(distances, shortestPathTreeSet);
 
-        // Mark the picked vertex as processed
-        sptSet[u] = true;
+        // Marca o vértice escolhido como incluso na árvore
+        shortestPathTreeSet[minimumDistanceVertexIndex] = true;
 
-        // Update dist value of the adjacent vertices of the
-        // picked vertex.
-        for (int v = 0; v < V; v++)
+        // Atualiza o valor de distância dos vértices adjacentes
+        for (int v = 0; v < VERTICES_COUNT; v++)
 
-            // Update dist[v] only if is not in sptSet,
-            // there is an edge from u to v, and total
-            // weight of path from src to v through u is
-            // smaller than current value of dist[v]
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
-                dist[v] = dist[u] + graph[u][v];
+            // Atualiza distances[v] somente se:
+            // - Vértice v não está no conjunto da árvore de caminho mais curto,
+            // - Existe uma aresta de minimumDistanceVertexIndex para v,
+            // - A distância total do caminho de source para v, passando por minimumDistanceVertexIndex,
+            // é menor do que o valor atual de distances[v]
+            if (!shortestPathTreeSet[v] &&
+                graph[minimumDistanceVertexIndex][v] &&
+                distances[minimumDistanceVertexIndex] != INT_MAX &&
+                distances[minimumDistanceVertexIndex] + graph[minimumDistanceVertexIndex][v] < distances[v])
+            {
+                distances[v] = distances[minimumDistanceVertexIndex] + graph[minimumDistanceVertexIndex][v];
+            }
     }
 
-    // print the constructed distance array
-    printSolution(dist);
+    printSolution(distances);
 }
 
-// driver's code
 int main()
 {
-    int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
-                       {4, 0, 8, 0, 0, 0, 0, 11, 0},
-                       {0, 8, 0, 7, 0, 4, 0, 0, 2},
-                       {0, 0, 7, 0, 9, 14, 0, 0, 0},
-                       {0, 0, 0, 9, 0, 10, 0, 0, 0},
-                       {0, 0, 4, 14, 10, 0, 2, 0, 0},
-                       {0, 0, 0, 0, 0, 2, 0, 1, 6},
-                       {8, 11, 0, 0, 0, 0, 1, 0, 7},
-                       {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+    int graph[VERTICES_COUNT][VERTICES_COUNT] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                                                 {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                                                 {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                                                 {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                                                 {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                                                 {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                                                 {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                                                 {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                                                 {0, 0, 2, 0, 0, 0, 6, 7, 0}};
 
-    // Function call
     dijkstra(graph, 0);
 
     return 0;
 }
-
-// This code is contributed by shivanisinghss2110
